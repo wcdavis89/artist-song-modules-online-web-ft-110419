5aa0d627daf44a458af9d6f90165f22e408a1080
Whittney Davis <wcdavis89@yahoo.com>
2019-12-04 13:57:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,14 @@
 require 'pry'
 
 class Song
-
-  extend Memorable::ClassMethods
-  extend Findable::ClassMethods
-  include Memorable::InstanceMethods
-  include Paramable::InstanceMethods
-
+  
   attr_accessor :name
   attr_reader :artist
   
-  
+   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
 
   @@songs = []
 
